2b41378bccae48ff57330ab6f6b6d6fa73b04ec4
Raúl Reymundi <raul@kickstarter.com>
2022-07-22 21:52:37 -0400

    Done.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [status, setStatus] = useState(false)
+
+  const itemClass = status ? "in-cart" : ""
+  const buttonText = status ? 'Remove From Cart' : 'Add to Cart'
+  
+  function handleClick(){
+    setStatus(status => !status)
+  }
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{buttonText}</button>
     </li>
   );
 }
